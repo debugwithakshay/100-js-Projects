f5d7afc82f91c0b760aefea0b8cd49f2c4869ad0
Akshay Yadav <akshayyadavindia31@gmail.com>
2024-02-04 17:12:14 +0530

    added assets offline

--- 16_check_internet_connection/script.js
+++ 16_check_internet_connection/script.js
@@ -2,12 +2,34 @@ let alertBox = document.querySelector(".alert__box");
 let closeAlert = document.querySelector(".icon--close");
 let alertHeading = document.querySelector(".alert--heading");
 let alertDesc = document.querySelector(".alert--description");
-let alertImg = document.querySelector(".icon--network")
+let alertImg = document.querySelector(".icon--network");
+let reconnectBtn = document.querySelector(".button--primary");
+let timer = document.querySelector(".timer");
+let timerValue = 30;
+
+//reconnecting on timer
+function startTimer() {
+  var myTimer = setInterval(() => {
+    alertDesc.innerHTML = `Your network is unavailable. We will attempt to reconnect you in <span class="timer">${timerValue}</span> Seconds`;
+    if (timerValue == 0) {
+      timerValue = 30;
+      checkConnection();
+    }
+    timerValue--;
+  }, 1000);
+}
+
+//reconnecting while pressing reconnect button
+reconnectBtn.addEventListener("click", function () {
+  checkConnection();
+});
 
 //sending a request to checkout status
 const checkConnection = async () => {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await fetch(
+      "https://random-data-api.com/api/users/random_user?size=1"
+    );
     if (response.status == 200) {
       showonlineBox();
     }
@@ -23,8 +45,9 @@ function showonlineBox() {
   alertHeading.innerText = "Restored Connection";
   alertDesc.innerText = "Your device is now successfully to the internet";
   alertBox.style.borderTop = "3px solid rgb(79, 240, 79)";
-  alertImg.classList.replace("icon--offline", "icon--online")
-  alertImg.innerText = "wifi"
+  alertImg.classList.replace("icon--offline", "icon--online");
+  alertImg.src = "assets/online.svg";
+  reconnectBtn.classList.add("button--disabled");
 
   setTimeout(() => {
     alertBox.style.transform = "translate(-50%, -140px)";
@@ -32,13 +55,16 @@ function showonlineBox() {
 }
 
 //when connection close
-function showofflineBox(){
+function showofflineBox() {
   alertBox.style.transform = "translate(-50%, 0px)";
   alertHeading.innerText = "Lost Connection";
-  alertDesc.innerText = "Your device is now successfully to the internet";
-  alertBox.style.borderTop = "3px solid rgb(79, 240, 79)";
-  alertImg.classList.replace("icon--offline", "icon--offline")
-  alertImg.innerText = "wifi_off"
+  alertBox.style.borderTop = "3px solid rgb(237, 68, 68)";
+  alertImg.classList.replace("icon--online", "icon--offline");
+  alertImg.src = "assets/offline.svg";
+  reconnectBtn.classList.remove("button--disabled");
+  if (timerValue == "30") {
+    myTimer();
+  }
 }
 
 //closing alert box
