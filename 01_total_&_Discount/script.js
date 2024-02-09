b2e0402e20d5056d7cca7ed1e20a21dbd13e9170
Akshay Yadav <akshayyadavindia31@gmail.com>
2024-02-09 07:57:59 +0530

    added 01_total_&_discount

---
+++ 01_total_&_Discount/script.js
@@ -0,0 +1,8 @@
+let item1 = 212
+let item2 = 202
+let item3 = 11
+let item4 = 890
+let item5 = 234
+let discount = 150
+let total = (item1 + item2 + item3 + item4 + item5) - discount
+console.log("Your total is ", total);
