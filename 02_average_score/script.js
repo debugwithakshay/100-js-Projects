0b3f5e08f47c168506d12d2df9d0d160b3387b89
Akshay Yadav <akshayyadavindia31@gmail.com>
2024-02-09 08:11:29 +0530

    02_average_score/

---
+++ 02_average_score/script.js
@@ -0,0 +1,8 @@
+let player1 = 11
+let player2 = 25
+let player3 = 30
+let player4 = 15
+let player5 = 22
+
+let averageScore = (player1 + player2 + player3 + player4 + player5)/5
+console.log("Average score is ", averageScore);
