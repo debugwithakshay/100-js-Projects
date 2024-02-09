5b46b235a7eeb5609900305932db7799388bc1ac
Akshay Yadav <akshayyadavindia31@gmail.com>
2024-02-09 08:38:19 +0530

    renamed

---
+++ 03_multiple_writer/script.js
@@ -0,0 +1,11 @@
+let givenNumber = 3
+
+console.log(givenNumber,"x 1 =", givenNumber * 1);
+console.log(givenNumber,"x 2 =", givenNumber * 2);
+console.log(givenNumber,"x 3 =", givenNumber * 3);
+console.log(givenNumber,"x 4 =", givenNumber * 4);
+console.log(givenNumber,"x 5 =", givenNumber * 5);
+console.log(givenNumber,"x 6 =", givenNumber * 6);
+console.log(givenNumber,"x 8 =", givenNumber * 8);
+console.log(givenNumber,"x 9 =", givenNumber * 9);
+console.log(givenNumber,"x 10 =", givenNumber * 10);
