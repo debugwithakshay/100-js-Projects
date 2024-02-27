fe007c2a70c52a803f38ddc0406d64066175c86b
Akshay Yadav <akshayyadavindia31@gmail.com>
2024-02-27 08:43:59 +0530

    added project

---
+++ 04_remainder_finder/script.js
@@ -0,0 +1,6 @@
+let divisor = 3
+let divident = 20
+
+let quotient = (divident / divisor).toFixed(2)
+let remainder = divident % divisor
+console.log(divident, "is divided by", divisor, "which lead to quotient", quotient, "with remainder", remainder);
