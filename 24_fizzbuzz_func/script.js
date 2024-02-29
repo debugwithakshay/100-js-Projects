54153d80de9cb448b54eafd4dfb58d5ca1450038
Akshay Yadav <akshayyadavindia31@gmail.com>
2024-02-29 09:03:16 +0530

    added 24_fizzbuzz_func/

---
+++ 24_fizzbuzz_func/script.js
@@ -0,0 +1,11 @@
+function fizzBuzz(num) {
+  if (num % 3 == 0 && num % 5 != 0) {
+    console.log("fizz");
+  } else if (num % 5 == 0 && num % 3 != 0) {
+    console.log("buzz");
+  } else if (num % 3 == 0 && num % 5 == 0) {
+    console.log("fizzbuzz");
+  }
+}
+
+fizzBuzz(30);
