b8ec1177e4b6cbfc3c1439e3ae3b464879e14f06
Akshay Yadav <akshayyadavindia31@gmail.com>
2024-03-02 21:52:41 +0530

    added some new code

--- 32_text_to_speech/script.js
+++ 32_text_to_speech/script.js
@@ -1,1 +1,7 @@
-let baseSalary = 30000
+let userInput = document.querySelector("#textArea").value;
+let speakBtn = document.querySelector(".btn");
+
+speakBtn.addEventListener("click", function () {
+  let utterance = new SpeechSynthesisUtterance(userInput);
+  speechSynthesis.speak(utterance);
+});
