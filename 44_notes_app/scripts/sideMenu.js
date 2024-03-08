0392868577af1b061a9622a7f5c5e9ef9bc75b96
Akshay Yadav <akshayyadavindia31@gmail.com>
2024-03-08 12:57:55 +0530

    Modified files

--- 44_notes_app/scripts/sideMenu.js
+++ 44_notes_app/scripts/sideMenu.js
@@ -1,24 +1,42 @@
-const menuClose = document.querySelector(".icon--menuClose")
-const menuOpen = document.querySelector(".icon--menuOpen")
-const navbar = document.querySelector(".navbar")
-const linkBox = document.querySelector(".navigation-link-box")
-const defualtPage = document.querySelector(".default-page")
-
 
 // closing navbar
-menuClose.addEventListener("click", () => {
-    navbar.style.width = "0px"
-    menuClose.style.display = "none"
-    linkBox.style.display = "none"
-    defualtPage.style.width = "100%"
-    menuOpen.style.display = "inline-block"
-})
+menuClose.addEventListener("click", menuCloseFunc);
+function menuCloseFunc() {
+  navbar.style.width = "0px";
+  menuClose.style.display = "none";
+  linkBox.style.display = "none";
+  defualtPage.style.width = "100%";
+  viewnotePage.style.width = "100%";
+  addnotePage.style.width = "100%";
+  for (let icon of menuOpen) {
+    icon.style.display = "inline-block";
+  }
+}
 
 //opening navbar
-menuOpen.addEventListener("click", () => {
-    navbar.style.width = "80px"
-    menuClose.style.display = "inline-block"
-    linkBox.style.display = "flex"
-    defualtPage.style.width = "calc(100% - 80px)"
-    menuOpen.style.display = "none"
-})
+for (let icon of menuOpen) {
+  icon.addEventListener("click", menuOpenFunc);
+}
+
+function menuOpenFunc() {
+  navbar.style.width = "80px";
+  menuClose.style.display = "inline-block";
+  linkBox.style.display = "flex";
+  defualtPage.style.width = "calc(100% - 80px)";
+  viewnotePage.style.width = "calc(100% - 80px)";
+  addnotePage.style.width = "calc(100% - 80px)";
+  for (let icon of menuOpen) {
+    icon.style.display = "none";
+  }
+}
+
+//menu open-close during window resizing
+window.addEventListener("resize", screenSizeChecker);
+function screenSizeChecker() {
+  if (window.innerWidth <= 800) {
+    menuCloseFunc();
+  } else if (window.innerWidth > 800) {
+    menuOpenFunc();
+  }
+}
+screenSizeChecker();
